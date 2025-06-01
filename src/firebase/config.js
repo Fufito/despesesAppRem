c8b52aea64192b08cdf5ad5bbaae2438768403fd
Rafel <rafael_estarellas@yahoo.es>
2025-06-01 22:02:58 +0200

    Add files via upload

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+//import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.0/firebase-firestore.js";
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBoZf93AbZtPaJ501k4vbm7h59kKawYBF8",
+  authDomain: "dbdespeses.firebaseapp.com",
+  projectId: "dbdespeses",
+  storageBucket: "dbdespeses.firebasestorage.app",
+  messagingSenderId: "1098314871318",
+  appId: "1:1098314871318:web:cb1544c56bd6a1c1ff3b37"
+};
+//const app = initializeApp(firebaseConfig);
